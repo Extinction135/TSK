5070d378446af57a63632448db6ed699e6af7f80
MrGrak <someloudnoise@gmail.com>
2017-04-14 09:45:22 -0500

    bug: actor can dash while standing still. cause: code doens't check to make sure their is an input direction prior to setting dash state. solution: only allow dash when input direction is a valid cardinal or diagonal direction. updated Input.SetInputState() to only set Actor.InputState to dash if input.direction is a valid direction. renamed ComponentInput parameter in Input.MapPlayerInput() + SetInputState() + ResetInputData(). this makes it a bit clearer what the parameter is, in the context of Input.cs.

--- DungeonRun/Classes/Input.cs
+++ DungeonRun/Classes/Input.cs
@@ -154,37 +154,40 @@ namespace DungeonRun
             else { return false; }
         }
 
-        public static void ResetInputData(ComponentInput Input)
+        public static void ResetInputData(ComponentInput CompInput)
         {
-            Input.direction = Direction.None;
-            Input.attack = false;
-            Input.use = false;
-            Input.dash = false;
-            Input.interact = false;
+            CompInput.direction = Direction.None;
+            CompInput.attack = false;
+            CompInput.use = false;
+            CompInput.dash = false;
+            CompInput.interact = false;
         }
 
-        public static void MapPlayerInput(ComponentInput Component)
+        public static void MapPlayerInput(ComponentInput CompInput)
         {   //maps input helper state to input component state
             //AI sets input component state directly, without using a controller/input helper abstraction
-            ResetInputData(Component); //reset the component
-            Component.direction = gamePadDirection;
-            if (IsNewButtonPress(Buttons.X)) { Component.attack = true; }
-            else if (IsNewButtonPress(Buttons.Y)) { Component.use = true; }
-            else if (IsNewButtonPress(Buttons.B)) { Component.dash = true; }
-            else if (IsNewButtonPress(Buttons.A)) { Component.interact = true; }
+            ResetInputData(CompInput); //reset the component
+            CompInput.direction = gamePadDirection;
+            if (IsNewButtonPress(Buttons.X)) { CompInput.attack = true; }
+            else if (IsNewButtonPress(Buttons.Y)) { CompInput.use = true; }
+            else if (IsNewButtonPress(Buttons.B)) { CompInput.dash = true; }
+            else if (IsNewButtonPress(Buttons.A)) { CompInput.interact = true; }
         }
 
-        public static void SetInputState(ComponentInput Input, Actor Actor)
+        public static void SetInputState(ComponentInput CompInput, Actor Actor)
         {
             Actor.inputState = Actor.State.Idle; //reset inputState
-            Actor.compMove.direction = Input.direction; //set move direction
-            if (Input.direction != Direction.None) //actor must be moving
-            { Actor.inputState = Actor.State.Move; }
+            Actor.compMove.direction = CompInput.direction; //set move direction
+            if (CompInput.direction != Direction.None)
+            {   //if there is directional input, then the actor is moving
+                Actor.inputState = Actor.State.Move;
+                //the actor can only dash while moving
+                if (CompInput.dash) { Actor.inputState = Actor.State.Dash; }
+            }
             //determine + set button inputs
-            if (Input.attack) { Actor.inputState = Actor.State.Attack; }
-            else if (Input.use) { Actor.inputState = Actor.State.Use; }
-            else if (Input.dash) { Actor.inputState = Actor.State.Dash; }
-            else if (Input.interact) { Actor.inputState = Actor.State.Interact; }
+            if (CompInput.attack) { Actor.inputState = Actor.State.Attack; }
+            else if (CompInput.use) { Actor.inputState = Actor.State.Use; }
+            else if (CompInput.interact) { Actor.inputState = Actor.State.Interact; }
         }
 
     }
