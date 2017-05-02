9d8a0173b077c5c268757c294fb3fd6df6de09ee
MrGrak <someloudnoise@gmail.com>
2017-05-02 13:08:48 -0500

    added line to scale map sprite up x2. updated the offset to properly place the map sprite. playtested, various issues / bugs. it's much closer to where it should be, but it's still off a pixel horizontally. and the vertical height doesn't match the mockup. it's off by 8 pixels. which is weird and i don't really understand why right now. kind of rushing commits and code today due to time constraints.

--- DungeonRun/Classes/ScreenOverworld.cs
+++ DungeonRun/Classes/ScreenOverworld.cs
@@ -29,9 +29,9 @@ namespace DungeonRun
             map = new ComponentSprite(Assets.overworldSheet, 
                 new Vector2(window.border.position.X + 8, window.border.position.Y + 24), 
                 new Byte4(0, 0, 0, 0), new Byte2(241, 149));
-            
-            map.position.X += map.cellSize.x / 2;
-            map.position.Y += map.cellSize.y / 2;
+            map.scale = 2.0f;
+            map.position.X += map.cellSize.x;
+            map.position.Y += map.cellSize.y;
         }
 
         public override void HandleInput(GameTime GameTime)
